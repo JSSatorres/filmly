38985b02df580bc83c3604e470cdf50c21d9c752
jssatorres <juansataz.dev@gmail.com>
2023-02-05 09:09:01 +0100

    feat (inrefaces) Movies interfaces

---
+++ src/interfaces/movies.ts
@@ -0,0 +1,13 @@
+export interface Movies {
+  Title     :string,
+  Year      :number,
+  Rated     :string,
+  Released  :string,
+  Genre     :string,
+  Director  :string,
+  Actors    :string,
+  Plot      :string,
+  Language  :string,
+  Poster    :string,
+  imdbID    :string,
+}
