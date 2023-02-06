83e1de2ceb645882de44a1c524b0be4afab45156
jssatorres <juansataz.dev@gmail.com>
2023-02-06 14:21:44 +0100

    Feat (Api) add secret api

--- src/api/moviesApi.ts
+++ src/api/moviesApi.ts
@@ -1,12 +1,11 @@
 import axios from 'axios'
 
-
 const moviesApi = axios.create({
     baseURL: 'http://www.omdbapi.com/',
     params: {
         apikey:  import.meta.env.VITE_API_KEY_OMDb,
       }
 })
-//TODO: env
+
 export default moviesApi
 
