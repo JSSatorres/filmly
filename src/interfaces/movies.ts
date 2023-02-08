b2a14a25ba9da172a839fdf48affa6ebeeaa7653
jssatorres <juansataz.dev@gmail.com>
2023-02-08 18:29:45 +0100

    feat (interfaces) movies

--- src/interfaces/movies.ts
+++ src/interfaces/movies.ts
@@ -1,4 +1,4 @@
-export interface Movies {
+export interface InfoMovie {
   Title     :string,
   Year      :number,
   Rated     :string,
@@ -10,5 +10,29 @@ export interface Movies {
   Language  :string,
   Poster    :string,
   imdbID    :string,
-  data      : { tototalResults : number }
+  data      :data
+}
+
+export interface Movies {
+Poster: string,
+Title : string,
+Type  : string,
+Year  : string,
+imdbID: string
+}
+
+export interface ApiResponse {
+  data: totalResults,
+  status: number,
+  statusText: string
+}
+
+interface totalResults {
+  totalResults: string
+  Search: Movies[]
+}
+interface data {
+  Response?        : 'True'| 'False'| null,
+  tototalResults?  : number | null
+
 }
