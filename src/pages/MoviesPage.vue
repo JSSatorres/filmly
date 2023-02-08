4e8c4c14655c86afca4fb7942e5600b7a61d4f2c
jssatorres <juansataz.dev@gmail.com>
2023-02-08 18:33:22 +0100

    Fix (MoviesPage) watc array

--- src/pages/MoviesPage.vue
+++ src/pages/MoviesPage.vue
@@ -1,59 +1,107 @@
 <template >
-    <Header />
-    <SimplePagination 
-      :totalPage='totalFound'
-      :currentPage='currentPage'
-      @pagechanged='changeCurrentPage'
+  <Modal
+    v-if="tabs[currentTab] !== ''"
+    @close="closeModal"
+    title="component.title"
+  >
+    <!-- :class-body="'mn-wd-prc__80'" -->
+    <component
+      :is="tabs[currentTab]"
+      :key="tabs[currentTab]"
+      :movie="movieInfo"
     />
-    <h1 v-if="moviesArr.length < 1">
+  </Modal>
+  <Header />
+  <SimplePagination 
+    :totalPage='totalFound'
+    :currentPage='currentPage'
+    @pagechanged='changeCurrentPage'
+  />
+  <div v-if=moviesArr.Error class="courses">
+    <div class="container">
+      <Title> <h2>{{moviesArr?.Error}}</h2> </Title>
+      <div class="container__image">
+        <img src="../assets/images/images.jpg" alt="try again">
+      </div>
+    </div>
+  </div>
+  <div v-else class="courses">
+    <div v-if="moviesArr.length < 1">
       <Spinner />
-    </h1>
-    <div v-else class="courses">
-        <Title> Movies </Title>
-        <section class="courses__grid">
-          <Card 
-            v-for ='movie in moviesArr.data.Search'
-            :movies="movie"
-            :key="movie.imdbID"
-          /> 
-        </section>
-    </div>    
+    </div>
+    <div v-else>
+      <Title> <h2>Results: {{totalFound}}</h2> </Title>
+      <Title> 
+        <h3>showing results from {{currentPage * 10 -9 }} to {{lastElement }} </h3>
+      </Title>
+      <section class="courses__grid">
+        <Card 
+          v-for ='movie in moviesArr'
+          :movies="movie"
+          :key="movie.imdbID"
+            @click="openModal(movie.imdbID,'tab2')"
+        /> 
+      </section>
+    </div>
+  </div>   
 </template>
 
 <script setup lang='ts'>
 
-import { ref, onMounted, watch  } from 'vue';
-import { Movies } from '../interfaces/movies';
+import { ref, reactive, onMounted, watch, computed } from 'vue';
+import { Movies, InfoMovie } from '../interfaces/movies';
 import { useMoviesData } from '../composable/useMovieData';
 import getMoviesOptions from '../helpers/getMoviesOptions'
+import getMovieById from '../helpers/getMovieById'
 
 import Header from '../components/Header.vue'
 import Card from '../components/Card.vue'
+import ExtendCard from '../components/ExtendCard.vue'
+import Modal from '../components/ui/Modal.vue'
 import Title from '../components/ui/Title.vue'
 import Spinner from '../components/ui/Spinner.vue'
 import SimplePagination from '../components/ui/SimplePagination.vue'
 
-const moviesArr = ref<Movies[]>([])
+
 const {search,type,currentPage, totalFound, updatecurrentPage} = useMoviesData()
+
+const moviesArr = ref<Movies[]>([])
+const movieInfo = ref<InfoMovie>()
+
+const currentTab = ref<string>('tab1');
+const tabs =  { 
+  tab1: "",
+  tab2: ExtendCard
+}
+
 onMounted(() => getMoviesArray())
 
-const getMoviesArray = async () => { 
+const getMoviesArray = async () => {   
   moviesArr.value = await getMoviesOptions(search.value)
 }
-const changeCurrentPage= (value:number)=> {
+
+const changeCurrentPage = (value:number)=> {
   updatecurrentPage(value)
-  console.log(value);
-  
 }
-watch(search, (newValue, oldValue) => {
-  getMoviesArray()
-});
 
-watch(type, (newValue, oldValue) => {
-  getMoviesArray()
-});
+const lastElement = computed(()=>{
+  let numberTo = currentPage.value * 10
+  if (currentPage.value * 10 > totalFound.value) {
+      numberTo = currentPage.value * 9 + (totalFound.value - currentPage.value * 9) 
+  }
+  return numberTo
+})
 
-watch(currentPage, (newValue, oldValue) => {
+const openModal = async (imdbID:string, componentName:string): Promise<void> => {
+  currentTab.value = componentName
+  movieInfo.value = await getMovieById(imdbID)
+
+}
+const closeModal = (imdbID:string):void => {
+ currentTab.value = 'tab1';
+}
+
+watch([search, type,currentPage], ([newSearch, newType, newCurrentPage]) => {
   getMoviesArray()
 });
 
@@ -82,5 +130,31 @@ watch(currentPage, (newValue, oldValue) => {
     margin-bottom: 1.87rem;
     break-inside: avoid;
   }
+  .container{
+    padding: 4rem;
+    margin: auto;
+
+    &__image{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: auto;
+    }
+    img {
+      height: 60vh;
+    }
+  }
+}
+
+@media (max-width: 470px) {
+  .courses__grid {
+    grid-template-columns: repeat(auto-fill, minmax(12rem, 1fr)) ;
+  }
+  .courses{
+    height: 85vh;
+  }
+   img {
+      height: 40vh !important;
+    }
 }
 </style>
